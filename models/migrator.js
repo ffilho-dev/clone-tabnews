0bd6bc3a1ad13cc7a5e9b27fc15e6c6fa2f3a24f
Francisco Filho <fcofilho0809@gmail.com>
2025-09-16 16:17:42 -0300

    feat: create `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,60 @@
+import database from "infra/database.js";
+import { ServiceError } from "infra/errors";
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    return await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient: dbClient,
+    });
+  } catch (error) {
+    throw new ServiceError({
+      message: "Erro ao listar as migrações pendentes",
+      cause: error,
+    });
+  } finally {
+    dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    return await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+      dbClient: dbClient,
+    });
+  } catch (error) {
+    throw new ServiceError({
+      message: "Erro ao executar as migrações pendentes",
+      cause: error,
+    });
+  } finally {
+    dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
