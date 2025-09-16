3542a89b8bb4d7b87a9cc2ebcbccfd56626f9e8f
Francisco Filho <fcofilho0809@gmail.com>
2025-09-16 19:41:47 -0300

    feat: create method `runPendingMigrations` and `clearDatabase`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,4 +1,6 @@
 import retry from "async-retry";
+import database from "infra/database";
+import migrator from "models/migrator";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -15,8 +17,18 @@ async function waitForAllServices() {
   }
 }
 
+async function clearDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+async function runPendingMigrations() {
+  await migrator.runPendingMigrations();
+}
+
 const orchestrator = {
   waitForAllServices,
+  clearDatabase,
+  runPendingMigrations,
 };
 
 export default orchestrator;
