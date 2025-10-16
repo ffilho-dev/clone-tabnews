f5f60dde47e7f7f0c224bc67570e004ed6e3a810
Francisco Filho <fcofilho0809@gmail.com>
2025-10-16 08:16:42 -0300

    feat: send activation email after `user` registration

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -1,6 +1,7 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
 import user from "models/user.js";
+import activation from "models/activation";
 
 const router = new createRouter();
 
@@ -11,6 +12,9 @@ export default router.handler(controller.errorHandlers);
 async function postHandler(request, response) {
   const userInput = request.body;
   const newUser = await user.create(userInput);
+
+  const activationToken = await activation.create(newUser.id);
+  await activation.sendEmailToUser(newUser, activationToken);
 
   return response.status(201).json(newUser);
 }
---
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -0,0 +1,64 @@
+import activation from "models/activation";
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+  await orchestrator.deleteAllEmail();
+});
+
+describe("Use case: Registration Flow (all sucessful)", () => {
+  let createUserResponseBody;
+
+  test("Create user account", async () => {
+    const createUserResponse = await fetch(
+      "http://localhost:3000/api/v1/users",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "frankdev",
+          email: "frankdev@gmail.com",
+          password: "senhaTeste",
+        }),
+      },
+    );
+
+    expect(createUserResponse.status).toBe(201);
+
+    createUserResponseBody = await createUserResponse.json();
+
+    expect(createUserResponseBody).toEqual({
+      id: createUserResponseBody.id,
+      username: "frankdev",
+      email: "frankdev@gmail.com",
+      features: ["read:activation_token"],
+      password: createUserResponseBody.password,
+      created_at: createUserResponseBody.created_at,
+      update_at: createUserResponseBody.update_at,
+    });
+  });
+
+  test("Receive activation email", async () => {
+    const lastEmail = await orchestrator.getLastEmail();
+
+    const activationToken = await activation.findOneByUserId(
+      createUserResponseBody.id,
+    );
+
+    expect(lastEmail.sender).toBe("<contato@franciscofilhodev.com.br>");
+    expect(lastEmail.recipients[0]).toBe("<frankdev@gmail.com>");
+    expect(lastEmail.subject).toBe("Ative seu cadastro no sistema!");
+    expect(lastEmail.text).toContain("frankdev");
+    expect(lastEmail.text).toContain(activationToken.id);
+  });
+
+  test("Activate account", async () => {});
+
+  test("Longin", async () => {});
+
+  test("Get user information", async () => {});
+});
